0ed4970f694703dfea3a875cf4e9284ebe472551
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-10 21:47:45 +0300

    connect to mongotlas

--- controllers/newPetController.js
+++ controllers/newPetController.js
@@ -5,6 +5,12 @@ const Pet = require('../models/PetModel.js')
 const newPet = async(req, res)=> {
     try {
         console.log(JSON.parse(req.body.newPet))
+        // const pets = require('../Pets.json')
+        // // console.log(pets);
+        // for (let pet of pets) {
+        //     const {avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter } = pet;
+        //      await Pet.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
+        // }
     //     const {avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter } = req.body;
     //     const pet = await Pet.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
     // 
@@ -21,7 +27,7 @@ const newPet = async(req, res)=> {
     // });
     // console.log(req.files.avatar)
     // console.log( newFileName)
-    // res.status(200).json(pet)
+    res.status(200).json(req.body)
 
     } catch(error) {
         console.error(error.message)
