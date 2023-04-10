e5c80dc54af69f289310abf9b5122e18f8564d91
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-10 22:15:20 +0300

    refactoring

--- controllers/petIdcontroller.js
+++ controllers/petIdcontroller.js
@@ -4,12 +4,9 @@ const PetId = async(req, res) => {
     // console.log('cookie', req.cookies)
     try{
         const pets = await Pet.find({})
-        // const pets = require('../Pets.json');
-        // console.log(pets);
-        // console.log(req.params.id)
         let pet = 'pet not found';
         for (let item of pets) {
-            // console.log(item.id)
+
             if (item.id === req.params.id) {
                 pet = item;
             }
