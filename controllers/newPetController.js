fb100bdeb31a6bece65b24d8ad772da89cfd0236
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-11 19:02:14 +0300

    create second BD for GP

--- controllers/newPetController.js
+++ controllers/newPetController.js
@@ -5,12 +5,6 @@ const Pet = require('../models/PetModel.js')
 const newPet = async(req, res)=> {
     try {
         // console.log(JSON.parse(req.body.newPet))
-        // const pets = require('../Pets.json')
-        // // console.log(pets);
-        // for (let pet of pets) {
-        //     const {avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter } = pet;
-        //      await Pet.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
-        // }
     //     const {avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter } = req.body;
     //     const pet = await Pet.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
     // 
--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
-const Pet = require('../models/PetModel.js');
+// const Pet = require('../models/PetModel.js');
+const Pet2 = require('../models/PetModel2.js')
 
 const listPets = async(req, res) => {
         try {
-            const pets = await Pet.find({})
-            // const pets = require('../Pets.json')
+            // const pets = await Pet.find({})
+            const pets = await Pet2.find({})
+            // const pets = require('../Petscopy.json')
             // console.log(pets)
             // for(let pet of pets) {
             //     const {avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter } = pet;
-            //     await Pet.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
+            //     await Pet2.create({avatar, type, gender, name, age, breeds, weight, height, sterilized, vaccinated, features, bio, shelter });
             // }
             res.status(200).json(pets)
         }
