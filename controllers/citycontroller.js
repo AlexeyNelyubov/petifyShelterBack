bd3c5c325448cd1cd2440f4b3b6a713dd1868fad
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-11 12:57:06 +0300

    fix bags

--- controllers/citycontroller.js
+++ controllers/citycontroller.js
@@ -1,16 +1,12 @@
 const listCities = (req, res) => {
-        try {
-            console.log(req)
-            const cities = require('../RussiaCities.json')
-            res.status(200).json(cities)
-        }
-        catch(error) {
-            console.error(error);
-            res.status(500).json([])
-        }
+    try {
+        const cities = require('../RussiaCities.json')
+        res.status(200).json(cities)
+    }
+    catch(error) {
+        console.error(error);
+        res.status(500).json([])
+    }
 }
 
 module.exports = { listCities }
-
-
-
