9db5153faea31dc360df0bace9099290af877db9
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-05-14 12:33:58 +0300

    delite localHost from app.listen

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use ('/api/v1', appRouter);
 
 mongoose.connect(`${process.env.MONGO_URI}`)
     .then(()=> {
-        app.listen(process.env.PORT, 'localhost', (error)=> {
+        app.listen(process.env.PORT, (error)=> {
             error ? console.log(error) : console.log (`connected to DB & server listening port http://localhost:${process.env.PORT}`);
             //         error ? console.log(error) : console.log (`connected to DB & server listening port http://127.0.0.1:${process.env.PORT}`);
         })
